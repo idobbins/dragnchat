aa659a9599f2079ca5e47843f70e71369d120d34
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-06-13 23:32:13 -0600

    chat node card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,15 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
@@ -16,12 +25,14 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator"
+
 import { ChevronsUpDown, Flame, SquarePlus } from "lucide-react";
 
 export default async function Home() {
 	return (
-		<main className="flex bg-gray-50 w-screen h-screen flex-col p-4">
-			<div className="flex items-center justify-between">
+		<main className="flex bg-none w-screen h-screen flex-col">
+			<div className="flex items-center justify-between p-4">
 				<Breadcrumb>
 					<BreadcrumbList>
 						<BreadcrumbItem>
@@ -65,6 +76,15 @@ export default async function Home() {
 						</DropdownMenuContent>
 					</DropdownMenu>
 				</div>
+			</div>
+			<div className="flex-1 flex w-screen h-screen bg-gray-50 absolute top-0 left-0 -z-10">
+				<Card className="w-96 h-96 relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+					<CardContent className="flex flex-col items-center justify-center h-full">
+						<div className="flex-1 w-full">Input</div>
+						<Separator />
+						<div className="flex-1 w-full">Output</div>
+					</CardContent>
+				</Card>
 			</div>
 		</main>
 	);
