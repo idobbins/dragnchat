420c5ea49e9fd49691a0abc2e32b809d30fc2429
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-06-15 15:33:27 -0600

    adding back simple editor

---
+++ src/app/_components/editor/editor.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from 'react';
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  BackgroundVariant,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from '@xyflow/react';
+import type { 
+  Node, 
+  Edge, 
+  Connection 
+} from '@xyflow/react';
+ 
+import '@xyflow/react/dist/style.css';
+ 
+interface NodeData extends Record<string, unknown> {
+  label: string;
+}
+
+type CustomNode = Node<NodeData>;
+type CustomEdge = Edge;
+
+const initialNodes: CustomNode[] = [
+  { id: '1', position: { x: 100, y: 100 }, data: { label: '1' } },
+  { id: '2', position: { x: 100, y: 200 }, data: { label: '2' } },
+];
+const initialEdges: CustomEdge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+ 
+export function Editor(): React.ReactElement {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+ 
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div className="absolute top-0 left-0 h-screen w-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 
 import { useAuth } from "@clerk/nextjs";
 import { AppHeader } from "./_components/layout/appheader";
+import { Editor } from "./_components/editor/editor";
 
 export default function Home() {
   const { isSignedIn } = useAuth();
 
   return (
-    <main className="flex h-screen w-screen flex-col">
+    <main className="relative flex h-screen w-screen flex-col">
       <AppHeader isSignedIn={!!isSignedIn} />
+      <Editor />
     </main>
   );
 }
