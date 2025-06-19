47941a25b7c0ad2c1833afe611722ca9eed25b7d
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-06-19 00:15:21 -0600

    ripping out stuff I dont need

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,7 +6,4 @@ import { v } from "convex/values";
 // app will continue to work.
 // The schema provides more precise TypeScript types.
 export default defineSchema({
-  numbers: defineTable({
-    value: v.number(),
-  }),
 });
